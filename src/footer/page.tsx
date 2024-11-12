1e7708936b4627a01bc3ed2b6beb51a9e6fd6cff
Rabia <rabiamashkoor758@gmail.com>
2024-11-11 18:46:56 -0800

    this is my project

--- src/footer/page.tsx
+++ src/footer/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='mt-16 py-6 text-center font-semibold bg-blue-500'>
+    <div className='mt-20 py-6 text-center font-semibold bg-blue-500'>
           @RABIA | ALL rights reserved 2024
     </div>
   )
