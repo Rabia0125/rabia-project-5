f0a3945cfc05da4842a1333953e0904ab0eb53cd
Rabia <rabiamashkoor758@gmail.com>
2024-11-12 13:02:58 -0800

    This is my first Next.js portfolio website.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,12 +3,12 @@ import Navbar from '../navbar/page'
 
 const Home = () => {
   return (
-    <div id='home' className='min-h-screen bg-no-repeat bg-[url(/student-free-photo.jpg)] bg-left lg:bg-[24%] bg-cover'
+    <div id='home' className='min-h-screen bg-no-repeat bg-[url(/student-free-photo.jpg)] bg-left lg:bg-[15%] bg-cover'
     style={{backgroundSize: "35%"}} >
       <Navbar />
       <div className='container grid lg:grid-cols-2 h[cals(100vh-60px)]'>
         <div className='hidden lg:block'></div>
-        <div className='text-[80px] sm:text-[120px] font-bold leading-tight flex justify-center items-center'>
+        <div className='flex justify-center items-center text-[100px] sm:text-[125px] font-bold leading-tight'>
           <div>
           <p data-aos="zoom-in-up"> I am </p>
           <p className='text-blue-500' data-aos="zoom-in-up">Rabia</p>
